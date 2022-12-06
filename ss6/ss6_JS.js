9d294e79866c381fa33589ba3ef51d0ea4f21ad9
haitan2408 <haitan28102408@gmail.com>
2022-12-06 10:38:53 +0700

    haitt up demo variable

--- ss6/ss6_JS.js
+++ ss6/ss6_JS.js
@@ -1,4 +1,6 @@
+let a =5;
 function changeText() {
+    console.log(a);
     document.getElementById("c11").innerHTML = "<h2 style='color: red'>Bye bye c11</h2>"
     document.getElementById("c12").innerText = "Bye";
 }
